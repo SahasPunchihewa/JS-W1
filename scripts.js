5c135bc44e37ab57aad91a8efdbea1e3fed3ecc9
SahasPunchihewa <sahasmcg2995@gmail.com>
2021-03-06 15:29:15 +0530

    inizial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//alert("Hello World");
+
+document.writeln("Hello I Am New Line 1!");
+document.writeln("<br>Hello I Am New Line 2!<br>");
+
+var name='Sahas';
+var num='21';
+
+var username=prompt("Enter Usename","username");
+
+console.log("loging in");
+
+document.write(username);
+
+document.write(name);
